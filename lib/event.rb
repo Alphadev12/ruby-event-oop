5bd84120836b7341f623773ab4537862bbaa774f
Alpha <ravalasonn@gmail.com>
2019-07-22 16:47:12 +0300

    Décaler un événement

--- lib/event.rb
+++ lib/event.rb
@@ -9,7 +9,18 @@ class Event
 		@title = title_to_save
 		@attendes = attendees_to_save
 	end
+
+	def postpone_24h
+		@start_date = @start_date + (24 * 3600)
+	end
+
+	def get_start_date
+		puts @start_date
+	end
 end
 
 attendees = ["truc@machin.com", "bidule@chose.fr"]
-Event.new("2010-10-31 12:00", 30, "standup quotidien", attendees)
+my_event = Event.new("2010-10-31 12:00", 30, "standup quotidien", attendees)
+
+my_event.postpone_24h
+my_event.get_start_date
